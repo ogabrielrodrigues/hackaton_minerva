817a964f4eeca1afd3cbfb7285503093f34f03fe
ogabrielrodrigues <gabrielrodrigues409@outlook.com>
2023-12-28 18:39:02 -0300

    update routes

--- server/router/router.go
+++ server/router/router.go
@@ -6,12 +6,15 @@ import (
 	"github.com/go-chi/chi/v5"
 	_ "github.com/ogabrielrodrigues/hackaton-minerva/server/docs"
 	"github.com/ogabrielrodrigues/hackaton-minerva/server/instance"
+	"github.com/ogabrielrodrigues/hackaton-minerva/server/middleware"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func RegisterRoutes(rt *chi.Mux) {
 	rt.Mount("/api/v1/swagger", httpSwagger.WrapHandler)
 	rt.Mount("/api/v1/employee", employeeRoutes())
+	rt.Mount("/api/v1/feedback", feedbackRoutes())
+	rt.Mount("/api/v1/answer", answersRoutes())
 }
 
 func employeeRoutes() http.Handler {
@@ -21,6 +24,36 @@ func employeeRoutes() http.Handler {
 	er.Post("/", ei.Create)
 	er.Get("/list", ei.List)
 	er.Get("/{registry}", ei.FindByRegistry)
+	er.Post("/auth", ei.Authenticate)
 
 	return er
+}
+
+func feedbackRoutes() http.Handler {
+	fr := chi.NewRouter()
+	fi := instance.NewFeedbackInstance()
+
+	fr.Group(func(r chi.Router) {
+		r.Use(middleware.AuthMiddleware)
+
+		r.Post("/", fi.Create)
+		r.Get("/", fi.List)
+		r.Get("/filter", fi.Filter)
+	})
+
+	return fr
+}
+
+func answersRoutes() http.Handler {
+	ar := chi.NewRouter()
+	ai := instance.NewAnswerInstance()
+
+	ar.Group(func(r chi.Router) {
+		r.Use(middleware.AuthMiddleware)
+
+		r.Get("/", ai.List)
+		r.Post("/reply", ai.Reply)
+	})
+
+	return ar
 }
